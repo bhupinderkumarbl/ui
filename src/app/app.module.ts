bf77a2bb3cf36297a7da93203b7951cb4f7a354d
Bhupinder <bhupinder.kumar@blinfosoft.com>
2018-02-10 21:44:18 +0530

    responsive changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,9 +18,6 @@ import {ReactiveFormsModule} from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { PostBlogComponent } from './post-blog/post-blog.component';
-
-
-
 const appRoutes: Routes = [
   {path: '', component: HomepageComponent},
   {path: 'HomepageComponent', component: HomepageComponent},
